5412be26e8ca109aca76654240b4fdfad002a426
danenberg15-star <danenberg15@gmail.com>
2026-04-10 02:43:27 -0700

    Fix: Added correct Firebase Database URL

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,15 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyArzxTaSFXwd4-bOVj3ggqGjYtb7e2ef0w",
   authDomain: "trivia-time-challenge.firebaseapp.com",
-  databaseURL: "https://trivia-time-challenge-default-rtdb.firebaseio.com", // <-- השורה הקריטית שהייתה חסרה!
+  databaseURL: "https://trivia-time-challenge-default-rtdb.firebaseio.com/",
   projectId: "trivia-time-challenge",
   storageBucket: "trivia-time-challenge.firebasestorage.app",
   messagingSenderId: "156140186843",
   appId: "1:156140186843:web:c8ab2845f5b120a578f0fe"
 };
 
+// אתחול האפליקציה במידה ולא אותחלה כבר
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+// ייצוא מסד הנתונים לשימוש בשאר חלקי האפליקציה
 export const db = getDatabase(app);
